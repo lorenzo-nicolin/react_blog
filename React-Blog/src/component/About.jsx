b0261f0a512f42b7a967c03542985ddc8fa580d6
Lorenzo <lorenzogovender152@gmail.com>
2024-12-04 09:40:58 +0200

    added the add button

--- React-Blog/src/component/About.jsx
+++ React-Blog/src/component/About.jsx
@@ -1,16 +1,30 @@
 import { useState } from "react";
+import axios from "axios";
 
 function About() {
   const [post, setPost] = useState("");
   const [title, setTitle] = useState("");
 
-  const handleSubmitToPost = () => {};
+  const handleSubmitToPost = async () => {
+    console.log(post + " " + title);
+
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/api/save", {
+        title: title,
+        desc: post,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error posting data:", error.message);
+    }
+  };
 
   return (
     <>
       <div>
         <form action="">
           <input
+            placeholder="Desc"
             type="text"
             name=""
             value={post}
@@ -21,9 +35,10 @@ function About() {
           <br />
           <br />
           <input
+            placeholder="Title"
             type="text"
             name=""
-            value={post}
+            value={title}
             onChange={(e) => {
               setTitle(e.target.value);
             }}
--- React-Blog/src/component/BlogDetails.jsx
+++ React-Blog/src/component/BlogDetails.jsx
@@ -2,19 +2,45 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function BlogDetails() {
-  const [test, setTest] = useState("");
+  const [test, setTest] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("http://127.0.0.1:8000/api/");
         console.log(response.data);
+        setTest(response.data);
       } catch (error) {
-        // setError(error);
+        console.error(error);
+        setError(error);
       } finally {
-        // setIsLoading(false);
+        setIsLoading(false);
       }
     };
     fetchData();
   }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      {Array.isArray(test) && test.length > 0 ? (
+        test.map((item) => (
+          <div key={item.id}>
+            <h2>{item.Title}</h2>
+          </div>
+        ))
+      ) : (
+        <div>No data available.</div>
+      )}
+    </div>
+  );
 }
